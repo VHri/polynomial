cb0d21aaf691e6fef87d389eedd45b2116026de2
Hrishav Varma <v.hrishav@gmail.com>
2024-01-27 22:42:20 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,15 +4,19 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+class Equation:
+    def evaluate(self, i):
+        return eval(repr(self).replace("X", str(i)))
 
-class Int:
+class Int(Equation):
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
 
-class Add:
+class Add(Equation):
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
@@ -20,7 +24,7 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
     
-class Sub:
+class Sub(Equation):
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
@@ -28,7 +32,7 @@ class Sub:
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
 
-class Mul:
+class Mul(Equation):
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
@@ -42,7 +46,7 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
     
-class Div:
+class Div(Equation):
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
@@ -58,3 +62,4 @@ class Div:
 
 poly = Sub( Div( Int(4), Sub(Int(3), Mul(Int(1), Int(6)))), Add( X(), Div( Int(1), Sub( Div(X(), Int(2)), Int(3)))))
 print(poly)
+print(poly.evaluate(-1))
